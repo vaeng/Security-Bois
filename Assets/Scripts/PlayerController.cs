e161938e68b5b804be6354e1a0d99c2c36b5928a
Christian Willner <34183939+vaeng@users.noreply.github.com>
2024-11-08 21:36:10 +0100

    feat: let guests exit

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -17,5 +17,18 @@ public class PlayerController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed, Space.World);
         transform.Translate(Vector3.forward * verticalInput * Time.deltaTime * speed, Space.World);
+    }
+
+    // if a gameobject with tag "Guest" collides with the player,
+    //push the guest to the gameobject with the tag exit
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Guest"))
+        {
+            GameObject exit = GameObject.FindGameObjectWithTag("Exit");
+            // apply force to the guest and push it to the exit
+            Vector3 directionToExit = exit.transform.position - collision.gameObject.transform.position;
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(directionToExit.normalized * 1000);
+        }
     }
 }
