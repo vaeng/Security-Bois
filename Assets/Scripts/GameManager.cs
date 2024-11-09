9c712896748273dcb95eb7113f66377a58c133ea
Christian Willner <34183939+vaeng@users.noreply.github.com>
2024-11-09 13:34:57 +0100

    feat: add pause mode

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,14 +3,19 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public GameObject[] players;
+
+    public bool gameWon = false;
+    public int totalTimeInSeconds = 60;
+    public float secondsLeft;
     int activePlayerIndex = 0;
+
+    public bool isPaused = false;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // Take players that are already in the scene
-        Debug.Log("Number of players: " + players.Length);
         // activate the first player's PlayerController Script
         players[activePlayerIndex].GetComponent<PlayerController>().enabled = true;
+        secondsLeft = totalTimeInSeconds;
     }
 
     // Update is called once per frame
@@ -26,5 +31,37 @@ public class GameManager : MonoBehaviour
             }
             players[activePlayerIndex].GetComponent<PlayerController>().enabled = true;
         }
+        // count guests in the scene, if zero, game is won
+        if (GameObject.FindGameObjectsWithTag("Guest").Length == 0)
+        {
+            gameWon = true;
+            Debug.Log("Game Won!");
+        }
+
+        // count down the time in seconds:
+        secondsLeft -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.Joystick1Button7))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+            isPaused = !isPaused;
+        }
+    }
+
+    void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    void ResumeGame()
+    {
+        Time.timeScale = 1;
     }
 }
