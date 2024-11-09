1f7d4cfa88d3060c3b23384bb171c26661b2b6fc
Made Line <madeline.streubel@gmx.de>
2024-11-09 20:03:44 +0100

    added 4rth basemap, added characterselection

---
+++ Assets/Scripts/CharacterSeectionData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class CharacterSelectionData : MonoBehaviour
+{
+    public static GameObject player1Character;
+    public static GameObject player2Character;
+}
--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+    using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
@@ -6,5 +6,29 @@ public class SceneChanger : MonoBehaviour
     public void LoadScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    private bool player1Confirmed = false;
+    private bool player2Confirmed = false;
+
+    public void ConfirmPlayer1(GameObject selectedCharacter)
+    {
+        CharacterSelectionData.player1Character = selectedCharacter;
+        player1Confirmed = true;
+        CheckBothPlayersConfirmed();
+    }
+        public void ConfirmPlayer2(GameObject selectedCharacter)
+    {
+        CharacterSelectionData.player2Character = selectedCharacter;
+        player2Confirmed = true;
+        CheckBothPlayersConfirmed();
+    }
+        private void CheckBothPlayersConfirmed()
+    {
+        if (player1Confirmed && player2Confirmed)
+        {
+            // Beide Spieler haben bestätigt – Szene wechseln
+            LoadScene("levelSelection");
+        }
     }
 }
